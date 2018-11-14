a9a1e2fb193902cbb315645da198a8671867522c
Byhalf <gareth.lafay@gmail.com>
2018-11-14 21:59:39 +0100

    modification du main pour que les arguments soient donnés lors de l'execution

--- games/Main.java
+++ games/Main.java
@@ -6,28 +6,45 @@ public class Main{
   public static void main(String [] args){
     GamePlayer player1;
     GamePlayer player2;
+    int choixJeu =0;
+    int choixJoueur1=1;
+    int choixJoueur2=0;
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Joueur 1, robot(0) ou humain(1)?");
-    int choix = scanner.nextInt();
-    if(choix==0)
+
+    try {
+      if(args[0].equals("nim"))
+        choixJeu=0;      
+      else if(args[0].equals("morpion"))
+        choixJeu=1;
+      if(args[1].equals("humain"))
+        choixJoueur1=1;
+      else
+        choixJoueur1=0;
+      
+      if(args[2].equals("humain"))
+        choixJoueur2=1;
+      else
+        choixJoueur2=0;
+    }
+    catch(ArrayIndexOutOfBoundsException e){
+      System.out.println("Il faut donner des arguments au main jeu/typeJ1/typeJ2");
+      System.out.println("Par défaut jeu=nim J1=humain J2=robot");
+    }
+    
+    if(choixJoueur1==0)
       player1 = new RandomPlayer();
     else{
-      System.out.println("quel est le nom du joueur");
+      System.out.println("quel est le nom du joueur 1?");
       player1 = new Human(scanner.next());
     }
-
-    System.out.println("Joueur 2, robot(0) ou humain(1)?");
-    choix = scanner.nextInt();
-    if(choix==0)
+    if(choixJoueur2==0)
       player2 = new RandomPlayer();
     else{
-      System.out.println("quel est le nom du joueur");
-      player2 = new Human(scanner.next());
+    System.out.println("quel est le nom du joueur 2?");
+    player2 = new Human(scanner.next());
     }
 
-    System.out.println("Jouer au nim 0 ou TicTacToe 1?");
-    choix = scanner.nextInt();
-    if(choix==0){
+    if(choixJeu==0){
       System.out.println("quel est le nombre d'allumettes?");
       int initialNbMatches = scanner.nextInt();
       System.out.println("quel est le nombre max d'allumettes ramassés?");
@@ -38,7 +55,7 @@ public class Main{
 
 
     }
-    else{
+    else if(choixJeu==1){
       TicTacToe game = new TicTacToe(player1,player2);
       Orchestrator orch = new Orchestrator(game);
       orch.playGame();
